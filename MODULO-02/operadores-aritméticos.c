830d87fc0742c7154c50170210aab7be7998219f
Rangelzin <victorrangeltasse85@hotmail.com>
2025-07-12 16:08:34 -0300

    feat: operadores aritmeticos em C

---
+++ MODULO-02/operadores-aritméticos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+    int a = 3;
+    int b = 2;
+    int r = 0;
+    //  r = a + b; soma
+    //  r = a - b; subtração
+    //  r = a * b; multiplicação
+    //  r = a / b; divisão
+    //  r = a % b; módulo
+    //  r = -b; unario
+    printf("Resultado: %d\n", r);
+
+}
